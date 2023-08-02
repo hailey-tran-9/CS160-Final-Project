5ec67918e521ba4d4693aff9bc8ad7f098b73b32
hailey-tran-9 <101952089+hailey-tran-9@users.noreply.github.com>
2023-08-02 02:11:00 -0700

    Switch to GLTF

--- src/script.js
+++ src/script.js
@@ -1,49 +1,62 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Create the scene, camera, and renderer
+// Add scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color( 0xffffff );
+// scene.add(new THREE.AxesHelper(5))
 
-const renderer = new THREE.WebGLRenderer();
+// Add light
+const light = new THREE.PointLight()
+light.position.set(0.8, 1.4, 1.0)
+scene.add(light)
+
+const ambientLight = new THREE.AmbientLight()
+scene.add(ambientLight)
+
+// Add camera
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set( 0, 10, 20 );
+
+// Add renderer
+const renderer = new THREE.WebGLRenderer( {antialias:true} );
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 document.body.appendChild( renderer.domElement );
 
 // Add controls
 const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true
+controls.target.set(0, 1, 0)
 
-// FBX Loader
-const loader = new FBXLoader();
-loader.load( 'couch___wide.fbx', function ( object ) {
-    object.traverse( function ( child ) {
-        if ( child.isMesh ) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-        }
-    } );
-    scene.add( object );
+// GLTF Loader
+const loader = new GLTFLoader();
+loader.load( 'Desk.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+    gltf.animations; // Array<THREE.AnimationClip>
+    gltf.scene; // THREE.Group
+    gltf.scenes; // Array<THREE.Group>
+    gltf.cameras; // Array<THREE.Camera>
+    gltf.asset; // Object
+
+}, function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
 } );
-
-const light = new THREE.PointLight()
-light.position.set(0.8, 1.4, 0.5)
-scene.add(light)
-
-// Add light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
-
-camera.position.set( 0, 20, 200 );
-controls.update();
-
-// // Change scene background
-// scene.background = new THREE.Color( 0xffffff );
 
 // Create a render/animate loop
 function animate() {
+    requestAnimationFrame( animate );
     controls.update();
-
-	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
 animate();
