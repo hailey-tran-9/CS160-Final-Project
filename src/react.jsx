1074155619028f1c0534f2cda7a36ef01a912902
Hailey Tran <101952089+hailey-tran-9@users.noreply.github.com>
2023-08-04 00:17:00 -0700

    Add game popups

--- src/react.jsx
+++ src/react.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Canvas, extend } from '@react-three/fiber'
 import { PerspectiveCamera, OrbitControls } from '@react-three/drei'
+import { Outline } from '@react-three/postprocessing'
+import { BlendFunction, Resizer, KernelSize } from 'postprocessing'
 
 import Papa from 'papaparse'
 
@@ -24,10 +26,10 @@ document.getElementById( "settings-btn" ).onclick =
 function() {
     // console.log(settingsOpen);
     if (!settingsOpen) {
-        document.getElementById( "settings-popup" ).style.visibility = "visible";
+        document.getElementById( "settings-overlay" ).style.visibility = "visible";
         settingsOpen = true;
     } else {
-        document.getElementById( "settings-popup" ).style.visibility = "hidden";
+        document.getElementById( "settings-overlay" ).style.visibility = "hidden";
         settingsOpen = false;
     }
 };
@@ -38,10 +40,10 @@ document.getElementById( "keyboard-controls-btn" ).onclick =
 function() {
     // console.log(keyBoardControlsOpen);
     if (!keyBoardControlsOpen) {
-        document.getElementById( "keyboard-controls-popup" ).style.visibility = "visible";
+        document.getElementById( "keyboard-controls-overlay" ).style.visibility = "visible";
         keyBoardControlsOpen = true;
     } else {
-        document.getElementById( "keyboard-controls-popup" ).style.visibility = "hidden";
+        document.getElementById( "keyboard-controls-overlay" ).style.visibility = "hidden";
         keyBoardControlsOpen = false;
     }
 };
@@ -59,10 +61,13 @@ function App() {
 
                 <PerspectiveCamera makeDefault position={[7, 3, 0]} />
                 <Rooms position={[0, 0, 0]}  scale={[8, 8, 8]}/>
+
                 <Couch onPointerEnter={(e) => HighlightObject(e)} onPointerLeave={(e) => UnhighlightObject(e)} position={[-2.4, 0.28, -2.5]} rotation={[0, Math.PI, 0]} scale={[0.9, 0.9, 0.9]}/>
                 <Desk onPointerEnter={(e) => HighlightObject(e)} onPointerLeave={(e) => UnhighlightObject(e)} position={[0, 0, 0]}/>
+
+                <ElectricGuitar onClick={(e) => DisplayInfo( "[ME][TA]L" )} onPointerEnter={(e) => HighlightObject(e)} onPointerLeave={(e) => UnhighlightObject(e)} position={[-1.25, 0.125, -2.25]} rotation={[0, -Math.PI/2, 0]} scale={[0.04, 0.04, 0.04]} />
                 
-                <Controller onClick={(e) => DisplayInfo( "TEST" )} position={[0, 2, 0]} scale={[0.05, 0.05, 0.05]} />
+                {/* <Controller onClick={(e) => DisplayInfo( "TEST" )} position={[0, 2, 0]} scale={[0.05, 0.05, 0.05]} /> */}
 
                 <OrbitControls />
 
@@ -85,11 +90,22 @@ function UnhighlightObject (e) {
 }
 
 // Helper func for clicking on game models
+var gamePopupOpen = false;
 function DisplayInfo ( objName ) {
     for (let i in data) {
         // console.log(data[i]);
         if (data[i]["Game Title"] == objName) {
             console.log(data[i]);
+
+            var obj = document.getElementById( "game-popup" );
+            if (!gamePopupOpen) {
+                obj.style.visibility = "visible";
+                gamePopupOpen = true;
+            } else {
+                obj.style.visibility = "hidden";
+                gamePopupOpen = false;
+            }
+            
         }
     }
 
