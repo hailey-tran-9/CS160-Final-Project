b95c238a60ae883d46fbfad3138d95d470209b91
hailey-tran-9 <101952089+hailey-tran-9@users.noreply.github.com>
2025-08-11 04:06:35 -0700

    [Deploy] change base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ const isCodeSandbox =
 export default {
   root: "src/",
   publicDir: "../static/",
-  base: "./",
+  base: "/CS160-Final-Project/",
   server: {
     host: true,
     open: !isCodeSandbox, // Open if it's not a CodeSandbox
